f32a2bd34f71c47a3c9a4a20292300da0613087b
karinatabares1 <anakarinatabarescamargo707@gmail.com>
2024-10-11 14:25:32 -0500

    laboratorio12

---
+++ Labora3_1.java
@@ -0,0 +1,14 @@
+// Aplicación orientada a objetos usando 2 clases.
+public class Labora3_1 {
+	public static void main(String[] args) {
+		
+		Labora3_2 rectangulo;
+		
+		rectangulo = new Labora3_2();
+		
+		rectangulo.ingresarDatos();
+		rectangulo.calcularArea();
+	}
+}
+
+
